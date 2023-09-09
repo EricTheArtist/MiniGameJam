3374a2030c1582a4c92834f7c413cc275c43413c
Eric Gardiner <Ericgardiner10@gmail.com>
2023-09-09 15:55:30 +0200

    added mamello scripts

---
+++ Assets/Mamello/BounceState.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BounceStateSc : MonoBehaviour
+{
+    public PhysicMaterial noBounce;
+    public PhysicMaterial bounce;
+
+    public GameObject[] BounceObj;
+
+    CountDownTimer timer;
+    [SerializeField] GameObject GameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BounceObj= GameObject.FindGameObjectsWithTag("Bouncer");
+        timer = GameManager.GetComponent<CountDownTimer>();
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer.fullTime < 100)
+        {
+            SwitchBounce();
+        }
+    }
+
+    void SwitchBounce()
+    {
+        foreach (GameObject obj in BounceObj)
+        {
+            obj.GetComponent<Collider>().material = bounce;
+        }
+        
+    }
+}
---
+++ Assets/Mamello/ParemtControl.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParemtControl : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        var platformMovement = other.collider.GetComponent<TopDownMovementController>();
+        if (platformMovement != null)
+        {
+            platformMovement.SetParent(transform);
+        }
+
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        var platformMovement = other.collider.GetComponent<TopDownMovementController>();
+        if (platformMovement != null)
+        {
+            platformMovement.ResetParent();
+        }
+    }
+}
